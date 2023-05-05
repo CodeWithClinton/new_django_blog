f0cfdcf8577cf0ec98c7ac2acf7367df7ee62db5
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-05-05 21:15:53 +0100

    new chnages

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'g9ZOY50RY7Z3quxvDCho',
+        'HOST': 'containers-us-west-178.railway.app',
+        'PORT': '7344',
+
     }
+
 }
 
 
